fb0dff938665659a004c8bb9c6cf7bfd47add413
DANIEL FELIPE SANCHEZ GALEANO <PIPEGALEANO02@GMAIL.COM>
2023-06-15 22:03:12 -0500

    CODIGO STREAM TIEMPO REAL
    En este codigo se utiliza una funcion de reloj en tiempo real y se hacen modificaciones para adaptarlas al codigo trabajado anteriormente
    Se logra una transmision en tiempo real por medio de consola , las funciones agregadas son para refrescar, mostrar y cargar el stream

--- grafica.py
+++ grafica.py
@@ -1,6 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 import argparse
 import time
@@ -9,11 +6,20 @@ import brainflow.board_shim
 
 from brainflow.board_shim import BoardShim, BrainFlowInputParams,BoardIds,LogLevels
 from brainflow.data_filter import DataFilter, WindowOperations, WaveletTypes
+from datetime import datetime
+import tkinter as tk
+INTERVALO_REFRESCO_RELOJ = 9  # En milisegundos
 
 
+def obtener_hora_actual():
+    return datetime.now().strftime("%H:%M:%S")
 
 
-
+def refrescar_reloj():
+    #print("Refrescando!")
+    variable_data.set(data_canales(board.get_board_data(),2))
+    raiz.after(INTERVALO_REFRESCO_RELOJ, refrescar_reloj)
+   
 
 
 def cargar_board(puerto,Id_board):
@@ -31,32 +37,28 @@ def iniciar_stream(board):
     data = board.get_board_data()  # get all data and remove it from internal buffer
     return(data)
 
-
-def detener_stream(board):
-    board.stop_stream()
-    
-
 def data_canales(data,Id_Board):
-     
-    eeg_channels = BoardShim.get_eeg_channels(2)
-    
-    for count, channel in enumerate(eeg_channels):
-            print('Original data for channel %d:' % channel)
-            print(data[channel])
-            #plt.plot(data[channel],label= "EEC")
-            #plt.legend(loc="upper left")
-            #plt.title("Grafica EEC")
-            #plt.xlabel("Tiempo")
-            #plt.xlabel("Voltaje")
-            #plt.show()
-           
-        
+   print (data[2])
+   data_2=data[2]
+   return(data_2)
+
+
+
 board=cargar_board("COM5",2)
 data=iniciar_stream(board)
-time.sleep(2)
-board.stop_stream()
-board.release_session()
-data_canales(data,2)
+#time.sleep(2)
+#board.stop_stream()
+#board.release_session()
+#data_canales(data,2)
 
 
-#print(sample_rate)
+raiz = tk.Tk()
+variable_data = tk.StringVar(raiz, value=data_canales(data,2))
+raiz.etiqueta = tk.Label(
+    raiz, textvariable=variable_data, font=f"Consolas 10")
+raiz.etiqueta.pack(side="top")
+app = tk.Frame()
+raiz.title("Reloj en Tkinter - By Parzibyte")
+refrescar_reloj()
+app.pack()
+app.mainloop()
