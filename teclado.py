9e765575e67ff8db1916b19b7c67181ca7b995c4
IvanDanielVillegas <51217454+IvanDanielVillegas@users.noreply.github.com>
2023-07-07 16:47:35 -0500

    Update teclado.py

--- teclado.py
+++ teclado.py
@@ -1,39 +1,74 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QPushButton, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import QTimer
 
+# Configuración del teclado virtual
+teclado = [['A', 'B', 'C', 'D', 'E'],
+           ['F', 'G', 'H', 'I', 'J'],
+           ['K', 'L', 'M', 'N', 'Ñ'],
+           ['O', 'P', 'Q', 'R', 'S'],
+           ['T', 'U', 'V', 'W', 'X'],
+           ['Y','Z']]
 
-class AlphabetMatrixWidget(QWidget):
+# Clase para la ventana del teclado
+class TecladoVirtual(QWidget):
     def __init__(self):
         super().__init__()
+        self.initUI()
 
-        self.init_ui()
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
 
-    def init_ui(self):
-        layout = QGridLayout()
+        # Inicializar colores de filas y columnas
+        self.colores_filas = [QColor(255, 255, 255), QColor(200, 200, 200), QColor(255, 0, 0)]
+        self.colores_columnas = [QColor(255, 255, 255), QColor(200, 200, 200), QColor(0, 255, 0)]
 
-        alphabet = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
-        row_count = 5
-        column_count = 6
+        # Crear botones del teclado
+        self.botones = []
+        for fila in range(len(teclado)):
+            for columna in range(len(teclado[fila])):
+                boton = QPushButton(teclado[fila][columna])
+                grid.addWidget(boton, fila, columna)
+                boton.clicked.connect(self.boton_presionado)
+                self.botones.append(boton)
 
-        for i, letter in enumerate(alphabet):
-            row = i % row_count
-            column = i // row_count
-            button = QPushButton(letter)
-            if(i=="C"):
-                button.setStyleSheet("background-color: #FF0000; font-size: 50px")  # Cambiar el tamaño de las letras
-            else:
-                button.setStyleSheet("background-color: #FF0000; font-size: 50px")  # Cambiar el tamaño de las letras
-            layout.addWidget(button, row, column)
+        # Cambiar colores de filas y columnas con el tiempo
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.actualizar_colores)
+        self.timer.start(100)  # Cambiar cada 1000 ms (1 segundo)
 
-        self.setLayout(layout)
+        self.setWindowTitle('Teclado Virtual')
+        self.show()
+
+    def boton_presionado(self):
+        boton = self.sender()
+        letra = boton.text()
+        print("Letra seleccionada:", letra)
+
+    def actualizar_colores(self):
+        # Actualizar los colores de las filas
+        self.colores_filas = self.colores_filas[1:] + [self.colores_filas[0]]
+
+        # Actualizar los colores de las columnas
+        self.colores_columnas = self.colores_columnas[1:] + [self.colores_columnas[0]]
+
+        # Actualizar los colores de los botones
+        for fila in range(len(teclado)):
+            for columna in range(len(teclado[fila])):
+                indice = fila * len(teclado[fila]) + columna
+                if indice < len(self.botones):
+                    boton = self.botones[indice]
+                    color_fila = self.colores_filas[fila % len(self.colores_filas)]
+                    color_columna = self.colores_columnas[columna % len(self.colores_columnas)]
+                    color_final = QColor(color_fila.red(), color_columna.green(), color_fila.blue())
+                    boton.setStyleSheet("background-color: " + color_final.name())
 
 
+
+# Ejecutar la aplicación
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = QMainWindow()
-    alphabet_matrix_widget = AlphabetMatrixWidget()
-    window.setCentralWidget(alphabet_matrix_widget)
-    window.setWindowTitle('Matriz de letras del abecedario')
-    window.setStyleSheet("background-color: #000000")  # Establecer el fondo negro
-    window.show()
+    ventana = TecladoVirtual()
     sys.exit(app.exec_())
